1ee241db339f4b6062656011919f65cb234c01a3
AsimanGasimzada <asiman.qasimzade@gmail.com>
2026-01-12 11:41:53 +0400

    Add project files.

---
+++ MPA101-Simulation/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MPA101_Simulation.Areas.Admin.Controllers;
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
